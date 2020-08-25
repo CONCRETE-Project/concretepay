13c4f70a44cb491f873fa2701a689ae364fcef46
Fabrizio Amodio <ziofabry@hotmail.com>
2020-08-25 21:34:01 +0200

    Address page refactoring

--- src/app/pages/wallet/address/address.page.ts
+++ src/app/pages/wallet/address/address.page.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute } from "@angular/router";
 import { WalletStorageService } from "../../../services/storage/wallet/wallet.service";
 import { BlockbookService } from "src/app/services/blockbook/blockbook.service";
 import { OnGoingProcessService } from "src/app/services/on-going-process/on-going-process.service";
-import { Utxo } from "src/app/models/blockbook/blockbook";
+import { AddressBalance } from "src/app/models/blockbook/blockbook";
 
 @Component({
     selector: "app-address",
@@ -16,7 +16,7 @@ export class WalletAddressPage implements OnInit, OnDestroy {
     wallet: Wallet;
     credentials: CoinCredentials;
     paramsSub: Subscription;
-    AddressInfo: Utxo[] = [];
+    AddressInfo: AddressBalance[] = [];
     constructor(
         private route: ActivatedRoute,
         public walletServiceStorage: WalletStorageService,
@@ -43,13 +43,12 @@ export class WalletAddressPage implements OnInit, OnDestroy {
                 (coinCred) => coinCred.Coin === coin
             );
             this.onGoingProcesService.set("common.loading");
-            this.AddressInfo = await this.getUtxos();
-            console.log(this.AddressInfo);
+            this.AddressInfo = await this.getAddressesBalance();
             this.onGoingProcesService.clear();
         });
     }
 
-    async getUtxos() {
-        return await this.blockbookService.getUtxos(this.credentials);
+    async getAddressesBalance() {
+        return await this.blockbookService.getAddressesBalance(this.credentials);
     }
 }
